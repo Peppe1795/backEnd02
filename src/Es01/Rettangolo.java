42ca0b62ddd1b379d89ce82d30e121c7cef9150d
Peppe1795 <petrucci238@gmail.com>
2023-06-28 17:50:22 +0200

    sistemazione repo back end

--- src/Es01/Rettangolo.java
+++ src/Es01/Rettangolo.java
@@ -1,20 +1,20 @@
 package Es01;
 
 public class Rettangolo {
-int altezza;
-int larghezza;
+	int altezza;
+	int larghezza;
 
-public Rettangolo(int h, int l) {
-	this.altezza = h;
-	this.larghezza = l;
-}
+	public Rettangolo(int h, int l) {
+		this.altezza = h;
+		this.larghezza = l;
+	}
 
-public int perimetro() {
-return (this.altezza + this.larghezza)* 2;
-}
+	public int perimetro() {
+		return (this.altezza + this.larghezza) * 2;
+	}
 
-public int area() {
-	return this.larghezza * this.altezza;
-}
+	public int area() {
+		return this.larghezza * this.altezza;
+	}
 
 }
